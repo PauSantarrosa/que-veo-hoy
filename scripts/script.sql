891c66adb4683a7bb8275282a2ccb6415d5ed6b8
Pau Santarrosa <paulasantarrosa@gmail.com>
2020-05-10 21:55:02 -0300

    script db

--- scripts/script.sql
+++ scripts/script.sql
@@ -9,26 +9,38 @@ CREATE TABLE  pelicula  (
    puntuacion  int DEFAULT NULL,
    poster  varchar(300) DEFAULT NULL,
    trama  varchar(700) DEFAULT NULL,
-  PRIMARY KEY ( id ),
-  KEY  peli_pk  ( id )
+  PRIMARY KEY (`id`)
 );
 
 -- creacion de la tabla
 CREATE TABLE  genero  (
    id  int NOT NULL AUTO_INCREMENT,
    nombre  varchar(30) DEFAULT NULL,
-  PRIMARY KEY ( id ),
-  KEY  id_pk  ( id )
+   PRIMARY KEY (`id`)
 );
 
 -- alter tabla peliculas no decia nada la guía pero lo dejo por las dudas
 /*
 ALTER TABLE  peliculas.pelicula  
 ADD COLUMN genero_id  int NULL AFTER  trama ,
-ADD INDEX  fk_peli_genero_idx  ( genero_id  ASC) VISIBLE;
-
-ALTER TABLE  peliculas.pelicula  
-ADD CONSTRAINT  fk_peli_genero 
-  FOREIGN KEY ( genero_id )
-  REFERENCES  peliculas.genero ( id );
+ PRIMARY KEY (`id`),
+  FOREIGN KEY ( `genero_id` )
+  REFERENCES  peliculas.genero (`id` );
 */
+
+-- creacion de la tabla actor
+CREATE TABLE  actor  (
+   id  int NOT NULL AUTO_INCREMENT,
+   nombre  varchar(70) DEFAULT NULL,
+  PRIMARY KEY ( `id` )
+);
+
+-- creacion de la tabla actor_pelicula
+CREATE TABLE  actor_pelicula  (
+	id  int NOT NULL AUTO_INCREMENT,
+   actor_id  int NOT NULL,
+   pelicula_id int NOT NULL,
+   PRIMARY KEY (`id`),
+  FOREIGN KEY (`actor_id`) REFERENCES `peliculas`.`actor` (`id`),
+  FOREIGN KEY (`pelicula_id`) REFERENCES `peliculas`.`pelicula` (`id`)
+);
